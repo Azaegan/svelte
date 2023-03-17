a7597b4f0942c9c2f2999948a729288aafb45195
Ann-Triksden Süld <anntriksden.syld@gmail.com>
2023-03-17 15:07:35 +0200

    add 1703

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,51 @@
 <script>
   let counter = 0;
   let message = 'Hello Svelte';
-  let items = ['piim', 'sai', 'viin'];
-
+  let items = [
+    {name:'piim', done:false} 
+    {name:'sai', done:true}
+    {name:'viin', done:false}
+    {name: 'õlu', done:true}
+    ];
+    $: doneItems =  items.filter(item => item.done);
+    $: toDoItems =  items.filter(item => item.done);
   function add(){
-    items.push(message);
+    items.push({name:message, done: false});
     items = items;
     message = '';
   }
 </script>
-<button class="button is-primary" on:click={() => counter++}>Value is {counter}</button>
 <div class="content">
   <input type="text" class="input" bind:value={message}>
   <button class="button is-primary" on:click={add}>Add</button>
-  <h1>{message}</h1>
+  <ItemList bind:items={items} title="All Items"></ItemList>
+  <ItemList bind:items={doneItems} title="Done Items"></ItemList>
+  <ItemList bind:items={toDoItems} title="To Do Items"></ItemList>
+</div>
+  <h1>All Items</h1>
   <ul>
     {#each items as item}
-    <li>{item}</li>
+    <li>
+      {item.name}
+    <input type="checkbox" bind:checked={item.done}>
+  </li>
     {/each}
   </ul>
-</div>
+  <h1>Done Items</h1>
+  <ul>
+    {#each doneItems as item}
+    <li>
+      {item.name}
+    <input type="checkbox" bind:checked={item.done}>
+  </li>
+    {/each}
+  </ul>
+  <h1>Todo Items</h1>
+  <ul>
+    {#each toDoItems as item}
+    <li>
+      {item.name}
+    <input type="checkbox" bind:checked={item.done}>
+  </li>
+    {/each}
+
